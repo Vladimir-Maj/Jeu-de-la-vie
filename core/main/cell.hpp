187bc0102767bab0ba7aae47742f1acee4293f2d
mg-root <meunier.sasha12@gmail.com>
2025-12-03 13:54:20 +0000

    feat: Gestion des cellules

--- core/main/cell.hpp
+++ core/main/cell.hpp
@@ -1,15 +1,13 @@
-#include "position.hpp";
-
 class Cell {
   private:
     bool state;
-    Position position;
 
   public:
-    Cell(bool etat, Position pos) : state(state), position(pos) {};
+    Cell() : state(false) {};
+    Cell(bool s) : state(s) {};
 
-    bool getEtat() { return this->state; };
-    void setEtat(bool newState) { this->state = newState; };
+    bool getState() { return this->state; };
+    void setState(bool newState) { this->state = newState; };
     bool isAlive() { return false; };
     void toggleState() {};
 };
