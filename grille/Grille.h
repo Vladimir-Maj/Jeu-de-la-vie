e147e1c70e3b73972044f3d98a1dc6b710cc890a
vladimir <vladimir.majcher@viacesi.fr>
2025-12-03 15:58:41 +0100

    feat: implémentation de la lecture de la grille + lecture criture et de fichier en terminal

---
+++ grille/Grille.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include <SFML/Graphics.hpp>//ajout
+#include "cellule/Cellule.h"
+
+class Grille
+{
+    protected:
+    int x=0;
+    int y=0;
+    std::string lecture;
+    std::string ecriture;
+    std::string ligne;
+    public:
+    
+    std::vector<Cellule> cellules;
+
+    Grille() = default;
+    Grille(std::string lecture);
+    virtual void iteration(int n, bool choix);
+    virtual void afficher_Grille();
+    virtual void lireFichier();
+    virtual void ecrireFichier(int a);
+    virtual void clear();
+ //a faire test unitaire
+};
