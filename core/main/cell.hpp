7988c4a60604c2f8521d08ad4f4168c3fba0725c
mg-root <meunier.sasha12@gmail.com>
2025-12-04 14:55:39 +0000

    feat: Ajout d'un attribut et de méthodes lié aux cellules obstacles

--- core/main/cell.hpp
+++ core/main/cell.hpp
@@ -1,13 +1,15 @@
 class Cell {
   private:
     bool state;
+    bool obstacle;
 
   public:
-    Cell() : state(false) {};
-    Cell(bool s) : state(s) {};
+    Cell() : state(false), obstacle(false) {};
+    Cell(bool s, bool o) : state(s), obstacle(o) {};
 
     bool getState() { return this->state; };
     void setState(bool newState) { this->state = newState; };
-    bool isAlive() { return false; };
-    void toggleState() {};
+
+    bool isObstacle() { return this->obstacle; };
+    void setObstacle(bool obstacle) { this->obstacle = obstacle; };
 };
