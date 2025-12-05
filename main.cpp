4bec1458ef35fd84b754691db9f227166e98c66e
mg-root <meunier.sasha12@gmail.com>
2025-12-05 18:24:32 +0100

    Ajout d'un point virgule..

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
+#include "core/cmd.hpp"
 #include "core/gameEngine.hpp"
 #include "core/src/fileManager.hpp"
 #include <filesystem>
 #include <iostream>
 #include <memory>
 #include <string>
-#include "core/cmd.hpp"
 
 int main(int argc, char *argv[]) {
     cmd app(argc, argv);
     return app.run();
-}
+};
